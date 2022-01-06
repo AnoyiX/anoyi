44ce7eed7a04cfcb6d332d40cf59886bdf46fc6e
Anoyi <545544032@qq.com>
2022-01-06 10:20:27 +0800

    :recycle: Update pages UI

--- fontend/pages/video/index.tsx
+++ fontend/pages/video/index.tsx
@@ -1,37 +1,36 @@
-const BlogPage = () => {
+import AppHeader from "../../components/AppHeader"
+
+const VideoPage = () => {
 
   return (
     <div className='w-full p-10 flex flex-col space-y-6'>
 
-      <div className='flex flex-row flex-1 space-x-6'>
+      <AppHeader></AppHeader>
 
-        <div className='flex flex-col space-y-6'>
+      <div className="bg-gray-900 w-full flex flex-row rounded-lg shadow shadow-white">
+        <video src="" controls={true} className="flex flex-1 rounded-l-lg">
+        </video>
+        <div className="w-72 flex-none"></div>
+      </div>
 
-          <div className='bg-white w-72 rounded-lg shadow flex flex-col flex-1 space-y-4 p-4'>
-            {
-              ['视频封面', '视频封面'].map((item, index) => (
-                <div className="bg-gray-100 flex w-full h-72 rounded-lg items-center justify-center" key={index}>
-                  <div className="text-gray-400">{item}</div>
-                </div>
-              ))
-            }
-          </div>
+      <div className='flex flex-col space-y-6'>
 
-        </div>
-        
-        <div className="bg-gray-900 flex flex-1 flex-col rounded-lg shadow shadow-white">
-          <div className="flex-none h-16">
-
-          </div>
-          <video src="" controls={true} className="flex flex-1 rounded-b-lg">
-          </video>
+        <div className='bg-white w-full rounded-lg shadow flex flex-row space-x-4 p-4'>
+          {
+            ['视频封面', '视频封面', '视频封面', '视频封面'].map((item, index) => (
+              <div className="bg-gray-100 flex w-48 h-72 rounded-lg items-center justify-center" key={index}>
+                <div className="text-gray-400">{item}</div>
+              </div>
+            ))
+          }
         </div>
 
       </div>
 
     </div>
+
   )
 
 }
 
-export default BlogPage
+export default VideoPage
