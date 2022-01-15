0b5320b789d5729b380615324de7e155ad0490b6
Anoyi <545544032@qq.com>
2022-01-15 16:01:25 +0800

    :sparkles: video play url

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -14,4 +14,4 @@ app.add_middleware(
 )
 
 
-__all__ = ['app', 'blog', 'mongo']
+__all__ = ['app', 'blog', 'mongo', 'video']
---
+++ backend/common/__init__.py
@@ -0,0 +1,3 @@
+class UA:
+    iphone = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'}
+    mac = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
