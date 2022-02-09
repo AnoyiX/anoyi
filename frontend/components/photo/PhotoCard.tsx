4f0776f176c8326be74de69a4aac8aa57a3094d2
Anoyi <545544032@qq.com>
2022-02-09 17:28:39 +0800

    :dizzy: hover effects

--- frontend/components/photo/PhotoCard.tsx
+++ frontend/components/photo/PhotoCard.tsx
@@ -12,7 +12,9 @@ export default function PhotoCard({ photo, onPlay }: PhotoCardProps) {
 
     return (
         <div className="rounded-lg w-full shadow-lg">
-            <img src={photo.thumbnail} alt="" className="rounded-t-lg w-full h-64 object-cover hover:opacity-50" onClick={onPlay}/>
+            <div className="overflow-hidden rounded-t-lg">
+                <img src={photo.thumbnail} alt="" className="w-full h-64 object-cover cursor-pointer transition ease-in-out hover:scale-110" onClick={onPlay} />
+            </div>
             <div className="p-4 flex flex-row text-sm text-gray-700 justify-between items-center">
                 <a className="flex flex-fow gap-1 items-center" href={photo.address} target="_blank">
                     <Location className="h-4 w-4" />
