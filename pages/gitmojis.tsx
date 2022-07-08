58eb485ce8bb8ba0e1542b724e70e7b0d20a2c6c
Anoyi <545544032@qq.com>
2022-07-08 15:50:08 +0800

    🎨 Improve gitmojis description

--- pages/gitmojis.tsx
+++ pages/gitmojis.tsx
@@ -25,11 +25,29 @@ const Page = ({ data }) => {
 
       <AppHeader path={[{ name: 'Git Emojis' }]} />
 
-      <FullContainer>
+      <div className='bg-gradient-to-br from-gray-900 to-gray-500 w-full h-96 rounded-lg shadow relative p-16'>
 
-        <div className="text-center pt-16 pb-8 text-3xl font-mono font-semibold">
-          🤓 An emoji guide for your commit messages
+        <div className="h-full flex flex-col justify-center">
+          <p className="text-5xl tracking-wide">😊🤣😂😇😉😘😜🙄😡😰🤢😴</p>
+          <p className="text-white text-4xl font-semibold my-4">Git Commit Messages 表情包使用指南</p>
+          <p className="text-gray-400 text-sm"><i className="fa-solid fa-circle-info mr-2"></i>点击任意彩色表情或者黑色标题即可完成复制！</p>
+          <div className="relative w-fit mt-6">
+            <a href="https://github.com/AnoyiX/anoyi" target="_blank" className="relative p-0.5 inline-flex items-center justify-center font-semibold overflow-hidden group rounded-md">
+              <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+              <span className="relative px-4 py-2 transition-all ease-out bg-gray-700 rounded-md group-hover:bg-opacity-0 duration-400">
+                <span className="relative text-white"><i className="fa-brands fa-github mr-2"></i>查看演示</span>
+              </span>
+            </a>
+          </div>
         </div>
+
+        <div className="absolute right-0 bottom-0 w-1/2 overflow-hidden h-full">
+          <img src="https://upload-images.jianshu.io/upload_images/3424642-0d8e3cc6382bbba3.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/960" alt="" className="w-full rounded-tl-xl translate-x-1/3 translate-y-16 drop-shadow-[0_48px_48px_rgba(59,130,246,0.75)]" />
+        </div>
+
+      </div>
+
+      <FullContainer>
 
         <div className="grid grid-cols-4 p-8 gap-10">
           {
