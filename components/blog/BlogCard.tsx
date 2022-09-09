ce219316e9f6a1d100ec40c754b546d9a9a7b108
Anoyi <545544032@qq.com>
2022-09-09 17:28:04 +0800

    🐛 Fix blog cannot access

--- components/blog/BlogCard.tsx
+++ components/blog/BlogCard.tsx
@@ -5,16 +5,16 @@ export default function BlogCard({ blog }: {blog: TBlog}) {
 
     return (
         <div className="w-full flex flex-row py-4 gap-2">
-            <Link href={`/blog/${blog.slug}`}>
+            <a href={`/blog/${blog.slug}`} target='_blank' rel='noreferrer'>
                 <img src={blog.list_image_url + '?imageMogr2/auto-orient/strip|imageView2/1/w/256/h/160'} alt="" className="rounded-lg cursor-pointer hover:opacity-75 w-64 h-40 object-cover bg-slate-200" />
-            </Link>
+            </a>
             <div className="flex flex-col p-2 justify-between">
                 <div className="flex flex-col gap-2">
-                    <Link href={`/blog/${blog.slug}`}>
+                    <a href={`/blog/${blog.slug}`} target='_blank' rel='noreferrer'>
                         <div className="text-black text-xl cursor-pointer hover:text-blue-600">
                             {blog.title}
                         </div>
-                    </Link>
+                    </a>
                     <div className="text-gray-400 text-sm">
                         {blog.first_shared_at.substring(0, 19).replace('T', ' ')}
                     </div>
